e6531be71c764c1065242dc92f5beaf535460a95
C1F5D2DD <3238858647@qq.com>
2026-03-12 16:18:18 +0800

    3

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-
+from astrbot.api.event import filter, AstrMessageEvent
+from astrbot.api.message_components import Video
+print=logger.info
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
@@ -18,7 +20,12 @@ class MyPlugin(Star):
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        try:
+            music = Video.fromFileSystem(path="/root/shits/ttep.mp4")
+        except:
+            print("获取shit失败")
+            music=None
+        yield event.chain_result([music])
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
