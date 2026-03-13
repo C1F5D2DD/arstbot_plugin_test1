33a7d848568ccb4328afe72b0bde446ebe91a4d5
C1F5D2DD <3238858647@qq.com>
2026-03-13 15:28:02 +0800

    c

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class MyPlugin(Star):
             prompt=chat_message
         )
 
-        yield event.chain_result([Comp.Plain(llm_resp)])
+        yield event.chain_result([Comp.Plain(llm_resp.completion_text)])
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
