48814771840c673af3123b6fa3f9ed127fb061fc
C1F5D2DD <3238858647@qq.com>
2026-03-13 15:17:41 +0800

    c

--- main.py
+++ main.py
@@ -22,10 +22,18 @@ class MyPlugin(Star):
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
+
+        chain=[Comp.Image.fromURL(url='https://localhost/shits/mm.jpg')]
+        yield event.chain_result(chain)
+    @filter.command('c')
+    async def chat(self, event: AstrMessageEvent,usermessage:str):
         umo = event.unified_msg_origin
         provider_id = await self.context.get_current_chat_provider_id(umo=umo)
-        chain=[Comp.Image.fromURL(url='https://localhost/shits/mm.jpg'),Comp.Plain(f"umo:{umo},pid:{provider_id}")]
-        yield event.chain_result(chain)
+        llm_resp = await self.context.llm_generate(
+            chat_provider_id=provider_id,  # 聊天模型 ID
+            prompt=usermessage
+        )
+        yield event.chain_result([Comp.Plain(llm_resp)])
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
